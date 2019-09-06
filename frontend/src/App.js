64e30e393eb6864dcd32a3fbd7dd8004928966e5
Daniel Lister <dan.lister@gmail.com>
2019-09-06 15:01:43 -0400

    clean up tic tac toe layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
+
 import "./App.css";
 import MetaTicTacToe from "./metaTicTacToe/MetaTicTacToe";
+import { Container } from "semantic-ui-react";
 
 function App() {
   return (
-    <div>
-      <MetaTicTacToe></MetaTicTacToe>
-    </div>
+    <Container>
+      <MetaTicTacToe />
+    </Container>
   );
 }
 
--- frontend/src/metaTicTacToe/MetaTicTacToe.js
+++ frontend/src/metaTicTacToe/MetaTicTacToe.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import SubGame from "./SubGame";
 import { calculateWinner, calculateSubGameWinner } from "./winner";
+import { Button, Grid, Accordion } from "semantic-ui-react";
+import "./MetaTicTacToe.css";
 
 const emptyBoard = [...Array(9)].map(() => Array(9).fill(null));
 
@@ -61,37 +63,68 @@ export default function MetaTicTacToe() {
   }
 
   return (
-    <>
-      <div className="game">
-        <div className="meta-board">
-          {winner && <div className="meta-winner-overlay">{winner}</div>}
-          <div className="meta-board-row">
-            {renderSubGame(0)}
-            {renderSubGame(1)}
-            {renderSubGame(2)}
-          </div>
-          <div className="meta-board-row">
-            {renderSubGame(3)}
-            {renderSubGame(4)}
-            {renderSubGame(5)}
-          </div>
-          <div className="meta-board-row">
-            {renderSubGame(6)}
-            {renderSubGame(7)}
-            {renderSubGame(8)}
+    <Grid centered>
+      <Grid.Row>
+        <div className="game">
+          <div className="meta-board">
+            {winner && <div className="meta-winner-overlay">{winner}</div>}
+            <div className="meta-board-row">
+              {renderSubGame(0)}
+              {renderSubGame(1)}
+              {renderSubGame(2)}
+            </div>
+            <div className="meta-board-row">
+              {renderSubGame(3)}
+              {renderSubGame(4)}
+              {renderSubGame(5)}
+            </div>
+            <div className="meta-board-row">
+              {renderSubGame(6)}
+              {renderSubGame(7)}
+              {renderSubGame(8)}
+            </div>
           </div>
         </div>
-      </div>
-      <button
-        onClick={() => {
-          setBoard(emptyBoard);
-          setXIsNext(true);
-          setLastMove(null);
-        }}
-      >
-        Reset
-      </button>
-      <div>Next move: {xIsNext ? "X" : "O"}</div>
-    </>
+      </Grid.Row>
+      <Grid.Row columns={3}>
+        <Grid.Column></Grid.Column>
+        <Grid.Column>
+          <Button
+            floated="right"
+            primary
+            onClick={() => {
+              setBoard(emptyBoard);
+              setXIsNext(true);
+              setLastMove(null);
+            }}
+          >
+            Reset
+          </Button>
+          <div className="next-move">Next move: {xIsNext ? "X" : "O"}</div>
+        </Grid.Column>
+        <Grid.Column></Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Accordion
+          styled
+          panels={[
+            {
+              key: "how-to-play",
+              title: "How to play",
+              content:
+                "Meta Tic Tac Toe is similar to regular Tic Tac " +
+                "Toe except every square on the board is another game. " +
+                "In order to claim a square on the meta board you must " +
+                "win the game in the sub board. Which sub board you are " +
+                "allowed to play on is determined by your opponents last " +
+                "move. For example, if they played in the center square of " +
+                "the top left board, your next move must be somewhere in the " +
+                "center board. If you are sent to a board that has already been " +
+                "won or is full, you can play on the board of your choice."
+            }
+          ]}
+        ></Accordion>
+      </Grid.Row>
+    </Grid>
   );
 }
